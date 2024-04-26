267909761df8a6db1b37c4b68b97331e45f21cb7
elizar777 <ahmadbekovelizar390@gmail.com>
2024-04-26 17:38:29 +0600

    Конкурс

---
+++ homework/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from homework.models import Settings, Gallery, About, Contact, News, Team, Post, Service
+
+# Register your models here.
+admin.site.register(Settings)
+admin.site.register(Gallery)
+admin.site.register(About)
+admin.site.register(Contact)
+admin.site.register(News)
+admin.site.register(Team)
+admin.site.register(Post)
+admin.site.register(Service)
---
+++ homework/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from homework.models import Settings, Gallery, About, Contact, News,Post, Team, Service
+
+# Create your views here.
+def index(request):
+    settings = Settings.objects.latest("id")
+    return render(request, "index.html", locals())
+
+def gallery(request):
+    photo = Gallery.objects.latest("id")
+    return render(request, "gallery.html", locals())
+
+def about(request):
+    about = About.objects.latest("id")
+    return render(request, "about.html", locals())
+
+def contact(request):
+    contact = Contact.objects.latest("id")
+    return render(request, "contact.html", locals())
+
+def news(request):
+    news = News.objects.latest("id")
+    return render(request, "news.html", locals())
+
+def post(request):
+    post = Post.objects.latest("id")
+    return render(request, "post.html",locals())
+
+def team(request):
+    team = Team.objects.latest("id")
+    return render(request, "team.html", locals())
+
+def service(request):
+    service = Service.objects.all()
+    return render(request, "index.html", locals())
